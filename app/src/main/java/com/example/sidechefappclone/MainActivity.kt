cc4537b88501869d56d68123529276a933195b6b
ttakpotosu2 <105734117+ttakpotosu2@users.noreply.github.com>
2022-10-15 06:25:38 +0000

    Initial commit

--- app/src/main/java/com/example/sidechefappclone/MainActivity.kt
+++ app/src/main/java/com/example/sidechefappclone/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SideChefAppCLoneTheme {
-
                 WelcomeScreen(navController = rememberNavController())
             }
         }
